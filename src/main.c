611aa020b0c96dee4216bb17d0960fffbe9e2e37
Maxime Escamez <maxime-escamez@telecomnancy.net>
2016-10-25 12:23:32 +0200

    Merge branch 'master' into step2
    waiting for total merge

--- src/main.c
+++ src/main.c
@@ -6,75 +6,63 @@
 #include <signal.h>
 #include <fcntl.h>
 #include <getopt.h>
+#include "../headers/main.h"
+
 
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
+
+//in this order : ptar -x -l "test.txt"
+
+/*$if (argc < 2) {                               useless for now
     printf("Not enough arguments\n");
     printf("Usage: tarlist filename\n");
     exit(1);
-  }
+  }*/
 
 int opt;
+int xflag=0;
+int lflag=0;
+int pflag=0;
+int zflag=0;
 
-while((opt = getopt(argc, argv, "xlpz")) != -1) { // : if arg required, :: if arg optionnal
-  printf("avant switch %d", opt);
+while((opt = getopt(argc, argv, "xlpz")) != -1) {
   switch (opt) {
-    printf("avant switch %d", opt);
                case 'x':
+                   xflag=1;
                    printf("x\n");
                    break;
                case 'l':
+                   lflag=1;
                    printf("l\n");
                    break;
                case 'p':
+                   pflag=1;
                    printf("p\n");
                    break;
                case 'z':
+                   zflag=1;
                    printf("z\n");
                    break;
                case '?' :
                    printf("invalid argument\n");
                }
 }
-/*
-struct header_posix_ustar {
-                char name[100];
-                char mode[8];
-                char uid[8];
-                char gid[8];
-                char size[12];
-                char mtime[12];
-                char checksum[8];
-                char typeflag[1];
-                char linkname[100];
-                char magic[6];
-                char version[2];
-                char uname[32];
-                char gname[32];
-                char devmajor[8];
-                char devminor[8];
-                char prefix[155];
-                char pad[12];
-              };
 
   int fd;
-  struct header_posix_ustar ownstruct;
-  fd = open(argv[1], O_RDONLY, 0);
-  printf("The file descriptor %d is open \n", fd);
-  read(fd, &ownstruct, 512);
-  printf("name: %s\n",ownstruct.name);
-  printf("mode: %s\n",ownstruct.mode);
-  printf("uid: %s\n",ownstruct.uid);
-  printf("gid: %s\n",ownstruct.gid);
-  printf("size: %s\n",ownstruct.size);
+  fd = open(argv[argc-1], O_RDONLY, 0);
+  printf("The file descriptor is open: %d\n", fd);
+  ustar buffer;
+  read(fd, &buffer, 512);
+  printf("%s\n",buffer.name);
+  //printf("%s\n",buffer.size);
+  lseek(fd, 512, SEEK_CUR);
+  while (*buffer.size!=0) {
+    read(fd, &buffer, 512);
+    printf("%s\n",buffer.name);
+    //printf("%s\n",buffer.size);
+    lseek(fd, 512, SEEK_CUR);
+  }
 
-  //char buffer[512];
-  //read(fd, buffer, 512);
-  //printf("%s\n",buffer);
-  //read(fd, buffer, 512);
-  //printf("%s\n",buffer);
-  //printf("%s\n",&ma_struct);
-  */
   return 0;
 }
