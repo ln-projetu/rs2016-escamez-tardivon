ea453ce9c78f6b31bea19e25ab87ad299409ef59
Maxime Escamez <maxime-escamez@telecomnancy.net>
2016-10-25 18:05:48 +0200

    commit update etape 2
    rajout de l'exception si on oublie le fichier cible
    
    exceptions d’options plus tricky -> WOP

--- src/main.c
+++ src/main.c
@@ -5,13 +5,20 @@
 #include <string.h>
 #include <signal.h>
 #include <fcntl.h>
+#include <ctype.h>
 #include <getopt.h>
+#include <string.h>
 #include "../headers/main.h"
 
+//in this order :  ptar -x -c "test.txt"
+
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    printf("usage: tarlist <filename>\n");
-    exit(1);
+
+  char *tab;                                       // rajout de l'exception si on oublie le fichier cible
+  tab=argv[argc-1];
+  if (!(strncmp(&tab[0],"-",1))) {
+    printf("No file target\n");
+    exit(EXIT_FAILURE);
   }
 
 int opt;
@@ -30,22 +37,33 @@ while((opt = getopt(argc, argv, "xlp:z")) != -1) {
                    lflag=1;
                    break;
                case 'p':
-                   pflag=1;
-                   nb_threads=atoi(optarg);
+                   printf("optarg = '%s'\n",optarg); //if optarg n'est pas un int, ca dégage.
+                   if(isdigit((unsigned char) optarg)){
+                     pflag=1;
+                     nb_threads=atoi(optarg);
+                   }else{
+                     printf("Option -p requires an argument of int\n");
+                     exit(EXIT_FAILURE);
+                   }
                    break;
                case 'z':
                    zflag=1;
                    break;
                case '?' :
-                   if (optopt=='p'){
-                        printf ("Option -%c requires an argument.\n", optopt);
-                   }else{
-                     printf("invalid option\n");
-                  }
+                   //if (optopt=='p'){
+                  //      printf ("Option -%c requires an argument of int.\n", optopt);
+                   //}else {
+                     printf("must have -x -l -p or -z option\n");
+                  //}
                    exit(EXIT_FAILURE);
                }
 }
 printf ("xflag = %d, lflag = %d, pflag = %d, nb_threads = %d, zflag = %d \n", xflag, lflag, pflag, nb_threads, zflag);
+
+
+
+
+
 
 
   int fd;
@@ -57,7 +75,7 @@ printf ("xflag = %d, lflag = %d, pflag = %d, nb_threads = %d, zflag = %d \n", xf
   //printf("%s\n",buffer.size);
   lseek(fd, 512, SEEK_CUR);
   while (*buffer.size!=0) {
-    read(fd, &buffer, 512); //Note: pour visualiser les bytes %02X
+    read(fd, &buffer, 512);
     printf("%s\n",buffer.name);
     //printf("%s\n",buffer.size);
     lseek(fd, 512, SEEK_CUR);
