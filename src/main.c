6d9019a0d0e9f0232bd5e6f714aa26004bee2128
Maxime Escamez <maxime-escamez@telecomnancy.net>
2016-10-25 12:32:32 +0200

    merge step2 into master

--- src/main.c
+++ src/main.c
@@ -5,16 +5,48 @@
 #include <string.h>
 #include <signal.h>
 #include <fcntl.h>
+#include <getopt.h>
 #include "../headers/main.h"
 
-int main(int argc, char const *argv[]) {
+
+/*int main(int argc, char *argv[]) {
   if (argc < 2) {
-    printf("Not enough arguement\n");
+    printf("Not enough arguments\n");
     printf("Usage: tarlist filename\n");
     exit(1);
-  }
+  }*/
+
+int opt;
+int xflag=0;
+int lflag=0;
+int pflag=0;
+int zflag=0;
+
+while((opt = getopt(argc, argv, "xlpz")) != -1) {
+  switch (opt) {
+               case 'x':
+                   xflag=1;
+                   printf("x\n");
+                   break;
+               case 'l':
+                   lflag=1;
+                   printf("l\n");
+                   break;
+               case 'p':
+                   pflag=1;
+                   printf("p\n");
+                   break;
+               case 'z':
+                   zflag=1;
+                   printf("z\n");
+                   break;
+               case '?' :
+                   printf("invalid argument\n");
+               }
+}
+
   int fd;
-  fd = open(argv[1], O_RDONLY, 0);
+  fd = open(argv[argc-1], O_RDONLY, 0);
   printf("The file descriptor is open: %d\n", fd);
   ustar buffer;
   read(fd, &buffer, 512);
