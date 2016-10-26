17444751dec5ac284b5805756d4bf27cdfe57c77
Quentin TARDIVON <quentin.tardivon@gmail.com>
2016-10-26 11:02:36 +0200

    Listing functionning

--- src/main.c
+++ src/main.c
@@ -7,6 +7,23 @@
 #include <fcntl.h>
 #include <getopt.h>
 #include "../headers/main.h"
+#include <math.h>
+
+long long convertOctalToDecimal(int octalNumber)
+{
+    int decimalNumber = 0, i = 0;
+
+    while(octalNumber != 0)
+    {
+        decimalNumber += (octalNumber%10) * pow(8,i);
+        ++i;
+        octalNumber/=10;
+    }
+
+    i = 1;
+
+    return decimalNumber;
+}
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
@@ -52,15 +69,31 @@ printf ("xflag = %d, lflag = %d, pflag = %d, nb_threads = %d, zflag = %d \n", xf
   fd = open(argv[argc-1], O_RDONLY, 0);
   printf("The file descriptor is open: %d\n", fd);
   ustar buffer;
-  read(fd, &buffer, 512);
-  printf("%s\n",buffer.name);
-  //printf("%s\n",buffer.size);
-  lseek(fd, 512, SEEK_CUR);
-  while (*buffer.size!=0) {
+  int init = 1;
+  char dest[11];
+  int size = 0;
+
+  while(init) {
     read(fd, &buffer, 512); //Note: pour visualiser les bytes %02X
-    printf("%s\n",buffer.name);
-    //printf("%s\n",buffer.size);
-    lseek(fd, 512, SEEK_CUR);
+
+    strncpy(dest, buffer.size,11);
+    size = convertOctalToDecimal(atoi(dest));
+    //printf("size: %d\n",size);
+    //printf("flag: %s\n",buffer.typeflag);
+
+    if (size == 0) {
+       if (atoi(buffer.typeflag) == 5) {
+         printf("%s\n",buffer.name);
+       }
+       else {
+         init = 0;
+       }
+    }
+    else {
+      printf("%s\n",buffer.name);
+    }
+    //printf("Avancement de :%d\n",(int) (512* ceil((double)size/512.0))); //Problème avec les valeurs en octal
+    lseek(fd,(int) (512* ceil((double)size/512.0)), SEEK_CUR);
   }
 
   return 0;
