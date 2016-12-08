d274273d802b52dad6a20cfc3a39da62928aba5b
Quentin TARDIVON <quentin.tardivon@gmail.com>
2016-12-08 16:48:35 +0100

    Nouveau départ pour les threads

--- src/main.c
+++ src/main.c
@@ -53,34 +53,16 @@ int createFile(char* path, int size, int fd, int mode) {
     //Create a file
     int fd2 = open(path, O_CREAT | O_WRONLY, mode);
     char buffer[size-1];
-    //Prendre un mutex là
     read(fd, &buffer, size);
-    //Relacher un mutex ici
     write(fd2, &buffer, size);
     close(fd2);
     return 0;
 }
 
-void *createFileP(void* arg) {
-  argument *newarg = (argument*) arg;
-  //printf("Thread num: %p\n",(void *) pthread_self());
-  //printf("Path: %s, Size: %d, fd: %d, mode: %d\n",newarg->path, newarg->size, newarg->fd, newarg->mode);
-
-  //Create a file
-  //int fd2 = open(newarg->path, O_CREAT | O_WRONLY, newarg->mode); TODO
-  char buffer[(newarg->size)-1];
-
-  pthread_mutex_lock(&newarg->mutex);
-  read(newarg->fd, &buffer, newarg->size);
-  pthread_mutex_unlock(&newarg->mutex);
-
-  printf("Ecriture Thread: %s\n", newarg->path);
-  printf("thread: %p\n",(void *) pthread_self());
-
-  //write(fd2, &buffer, newarg->size); TODO
-  //fsync(fd2); TODO
-  //close(fd2); TODO
-  sem_post(newarg->sema);
+void* createFileP(void* data) {
+  char name[100];
+  strcpy(name, data);
+  printf("Ecriture de %s\n", name);
   pthread_exit(NULL);
 }
 
@@ -88,10 +70,6 @@ int main(int argc, char *argv[]) {
 
   int fd;
   fd = open(argv[argc-1], O_RDONLY, 0);
-
-
-
-
 
   if(argc<2){
     printf("No target file\n");
@@ -109,6 +87,7 @@ int lflag=0;
 int pflag=0;
 int zflag=0;
 int nb_threads=0;
+pthread_t p_thread;
 
 
 while((opt = getopt(argc, argv, "xlp:z")) != -1) {
@@ -138,10 +117,9 @@ while((opt = getopt(argc, argv, "xlp:z")) != -1) {
 }
   //printf ("xflag = %d, lflag = %d, pflag = %d, nb_threads = %d, zflag = %d \n", xflag, lflag, pflag, nb_threads, zflag);
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
-if(zflag){}
-//if(nb_threads==0){}
+if(pflag==0){}
+if(zflag==0){}
+if(nb_threads==0){}
 
 
   ustar buffer;
@@ -151,12 +129,10 @@ if(zflag){}
   char * timecrop;
   char timebuff[20];
   struct tm * timeinfo;
-  pthread_t thread[nb_threads];
-  sem_t * sema;
-  sema = sem_open("mysema",O_CREAT, S_IRUSR | S_IWUSR, nb_threads);
+
+
 
   while(init) {
-
     read(fd, &buffer, 512); //Note: pour visualiser les bytes %02X
     strncpy(dest, buffer.size,11);
     size = convertOctalToDecimal(atoi(dest));
@@ -170,9 +146,9 @@ if(zflag){}
       }
       else {
         //printf("%c\n", buffer.mode[3]);
-        if(atoi(buffer.typeflag)==5){
+        if(buffer.typeflag[0]=='5'){
           printf("d%s%s%s ",modeReading(buffer.mode+4),modeReading(buffer.mode+5), modeReading(buffer.mode+6));
-        }else if(atoi(buffer.typeflag)==2){
+        }else if(buffer.typeflag[0]=='2'){
           printf("l%s%s%s ",modeReading(buffer.mode+4),modeReading(buffer.mode+5), modeReading(buffer.mode+6));
         }else{
           printf("-%s%s%s ",modeReading(buffer.mode+4),modeReading(buffer.mode+5), modeReading(buffer.mode+6));
@@ -198,7 +174,7 @@ if(zflag){}
         printf("%s",buffer.name);
 
         //if(simlink) print('-> destination')
-        if(atoi(buffer.typeflag)==2){
+        if(buffer.typeflag[0]=='2'){
           printf(" -> %s\n",buffer.linkname);
         }else{
           printf("\n");
@@ -225,48 +201,47 @@ if(zflag){}
       perm = convertOctalToDecimal(perm);
 
 
-      if (atoi(buffer.typeflag) == 5) {
-        //mkdir(buffer.name, perm); TODO
-        printf("Ecriture PP: %s\n",buffer.name);
+      if (buffer.typeflag[0] == '5') {
+        //mkdir(buffer.name, perm);
+        printf("Ecriture de : %s\n", buffer.name);
 
         chown(buffer.name, atoi(buffer.uid), atoi(buffer.gid));
       }
-      else if (atoi(buffer.typeflag) == 2) {
+      else if (buffer.typeflag[0] == '2') {
         //symlink(buffer.linkname, buffer.name); TODO
-        printf("Ecriture PP: %s\n",buffer.name);
+        printf("Ecriture de %s\n", buffer.name);
 
         chown(buffer.name, atoi(buffer.uid), atoi(buffer.gid));
+
+        struct timeval timeval[2];
+        struct timeval const *t;
+        timecrop=strdup(buffer.mtime);
+        timeval[0].tv_sec = 0;
+        timeval[0].tv_usec = 0;
+        timeval[1].tv_sec = convertOctalToDecimal(atol(timecrop));
+        timeval[1].tv_usec = 0;
+        t = timeval;
+        lutimes (buffer.name, t);
       }
       else {
-        //En tête du fichier
-        if (pflag) {
-          argument* arg;
-          arg = malloc(nb_threads * sizeof(argument));
-          int i= 0;
-          printf("%p\n",sema); //Il s'arrête donc là
-          sem_wait(sema);
-          arg[i].fd = fd;
-          arg[i].mode = perm;
-          arg[i].path = buffer.name;
-          arg[i].size = size;
-          arg[i].mutex = mutex;
-          arg[i].sema = sema;
-          pthread_create(&thread[i], NULL, createFileP,(void *) &arg[i]);
-
+        if (pflag == 1) {
+          char bufferFile[size-1];
+          read(fd, &bufferFile, size);
+          char name[100];
+          strcpy(name, buffer.name);
+          pthread_create(&p_thread, NULL, createFileP, (void*)&name);
         }
         else {
-          createFile(buffer.name, size, fd, perm);
+          //createFile(buffer.name, size, fd, perm);
+
+          chown(buffer.name, atoi(buffer.uid), atoi(buffer.gid));
+
+          struct utimbuf new_times;
+          timecrop=strdup(buffer.mtime);
+          new_times.modtime = convertOctalToDecimal(atol(timecrop));
+          utime(buffer.name, &new_times);
         }
-
-
-        chown(buffer.name, atoi(buffer.uid), atoi(buffer.gid));
       }
-
-
-      struct utimbuf new_times;
-      timecrop=strdup(buffer.mtime);
-      new_times.modtime = convertOctalToDecimal(atol(timecrop));;
-      utime(buffer.name, &new_times);
 
 
 
@@ -284,59 +259,42 @@ if(zflag){}
     }
 
   }
-  //Le programme doit attendre la fin de tous les threads
-  init = 1;
-  fd = open(argv[argc-1], O_RDONLY, 0);
-  sem_close(sema);
-  while (init) {
-    read(fd, &buffer, 512); //Note: pour visualiser les bytes %02X
-    strncpy(dest, buffer.size,11);
-    size = convertOctalToDecimal(atoi(dest));
+  close(fd);
+  if (xflag == 1) {
+    init = 1;
+    int fd3 = open(argv[argc-1], O_RDONLY, 0);
+    while (init) {
 
-    if (strncmp(buffer.magic, "ustar",5) != 0) {
-      init = 0;
+      read(fd3, &buffer, 512); //Note: pour visualiser les bytes %02X
+      strncpy(dest, buffer.size,11);
+      size = convertOctalToDecimal(atoi(dest));
+
+      if (strncmp(buffer.magic, "ustar",5) != 0) {
+        init = 0;
+      }
+      if (xflag == 1) {
+        char* perm1 = buffer.mode+3;
+        int perm = atoi(perm1);
+        perm = convertOctalToDecimal(perm);
+
+
+        if (buffer.typeflag[0] == '5') {
+          struct utimbuf new_times;
+          timecrop=strdup(buffer.mtime);
+          new_times.modtime = convertOctalToDecimal(atol(timecrop));
+          utime(buffer.name, &new_times);
+
+
+
+        }
+
+        lseek(fd3,(int) (512* ceil((double)size/512.0)), SEEK_CUR);
+
+        //printf("Avancement de : %d\n", (int) (512* ceil((double)size/512.0)) - size);
+      }
     }
-    if (xflag == 1) {
-      char* perm1 = buffer.mode+3;
-      int perm = atoi(perm1);
-      perm = convertOctalToDecimal(perm);
-
-
-      if (atoi(buffer.typeflag) == 5) {
-        struct utimbuf new_times;
-        timecrop=strdup(buffer.mtime);
-        new_times.modtime = convertOctalToDecimal(atol(timecrop));
-        utime(buffer.name, &new_times);
-
-
-      }
-      else if (atoi(buffer.typeflag) == 2) {
-
-        struct timeval timeval[2];
-        struct timeval const *t;
-        timecrop=strdup(buffer.mtime);
-        timeval[0].tv_sec = 0;
-        timeval[0].tv_usec = 0;
-        timeval[1].tv_sec = convertOctalToDecimal(atol(timecrop));
-        timeval[1].tv_usec = 0;
-        t = timeval;
-        lutimes (buffer.name, t);
-
-      }
-      else {
-        struct utimbuf new_times;
-        timecrop=strdup(buffer.mtime);
-        new_times.modtime = convertOctalToDecimal(atol(timecrop));
-        utime(buffer.name, &new_times);
-
-        chown(buffer.name, atoi(buffer.uid), atoi(buffer.gid));
-      }
-
-      lseek(fd,(int) (512* ceil((double)size/512.0)) - size, SEEK_CUR);
-
-      //printf("Avancement de : %d\n", (int) (512* ceil((double)size/512.0)) - size);
-    }
-  }
+    close(fd3);
+}
 
   return 0;
 }
